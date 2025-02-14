83759c9c97e220ef6a4a423ce382e4565e272aa3
alfonc.lazri <alfonc.lazri@sisal.al>
2025-02-14 02:45:14 +0100

    tst

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,24 +1,17 @@
-// path: /config/env/production/database.ts
-
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-   process.env.DATABASE_URL
- );
-
 export default ({ env }) => ({
-  connection: {  
-    client: 'postgres',
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
-      ssl: {
-        ca: env('DATABASE_CA'),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: false, // Allow self-signed certs
+        },
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  });
+  
